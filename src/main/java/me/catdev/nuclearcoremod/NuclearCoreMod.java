cf8fde3f35db7a2372e8942d3bcff2807f195585
CatDev <real.catdev@gmail.com>
2023-10-22 21:14:44 +0200

    commit

--- src/main/java/me/catdev/nuclearcoremod/NuclearCoreMod.java
+++ src/main/java/me/catdev/nuclearcoremod/NuclearCoreMod.java
@@ -2,21 +2,17 @@ package me.catdev.nuclearcoremod;
 
 import com.mojang.logging.LogUtils;
 import me.catdev.nuclearcoremod.blocks.Blocks;
-import me.catdev.nuclearcoremod.entity.armor.RadiationSuitRenderer;
 import me.catdev.nuclearcoremod.items.Items;
-import me.catdev.nuclearcoremod.items.custom.RadiationSuitItem;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 import software.bernie.geckolib3.GeckoLib;
-import software.bernie.geckolib3.renderers.geo.GeoArmorRenderer;
 
 @Mod(NuclearCoreMod.MOD_ID)
 public class NuclearCoreMod
@@ -46,7 +42,7 @@ public class NuclearCoreMod
     public static class ClientModEvents  {
         @SubscribeEvent
         public static void registerArmorRenderers(final EntityRenderersEvent.AddLayers event) {
-            GeoArmorRenderer.registerArmorRenderer(RadiationSuitItem.class, new RadiationSuitRenderer());
+
         }
     }
 }
