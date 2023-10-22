ff5a1fdb9615224618771cb3e26288f6bb208195
CatDev <real.catdev@gmail.com>
2023-10-22 13:13:52 +0200

    added uranium_ingot

--- src/main/java/me/catdev/nuclearcoremod/items/Items.java
+++ src/main/java/me/catdev/nuclearcoremod/items/Items.java
@@ -12,8 +12,8 @@ public class Items {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, NuclearCoreMod.MOD_ID);
 
-    public static final RegistryObject<Item> TEST_ITEM = ITEMS.register("test", () -> new Item(new Item.Properties().tab(CreativeTabs.TEST_TAB)));
     public static final RegistryObject<Item> RADIATION_METER = ITEMS.register("radiationmeter", () -> new Item(new Item.Properties().tab(CreativeTabs.TEST_TAB)));
+    public static final RegistryObject<Item> URANIUM_INGOT = ITEMS.register("uranium_ingot", () -> new Item(new Item.Properties().tab(CreativeTabs.TEST_TAB)));
 
     public static void Register(IEventBus eventBus) {
         ITEMS.register(eventBus);
