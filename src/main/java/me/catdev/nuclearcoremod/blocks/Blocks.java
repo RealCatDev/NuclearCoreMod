7f1d4aab0c8ee2013ca1666292e0f888e4a2a95b
CatDev <real.catdev@gmail.com>
2023-10-22 12:17:32 +0200

    Tried fixing radiation meter (with no results) and added Uranium Ore

--- src/main/java/me/catdev/nuclearcoremod/blocks/Blocks.java
+++ src/main/java/me/catdev/nuclearcoremod/blocks/Blocks.java
@@ -1,11 +1,16 @@
 package me.catdev.nuclearcoremod.blocks;
 
 import me.catdev.nuclearcoremod.NuclearCoreMod;
+import me.catdev.nuclearcoremod.items.CreativeTabs;
 import me.catdev.nuclearcoremod.items.Items;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -16,6 +21,10 @@ import java.util.function.Supplier;
 public class Blocks {
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, NuclearCoreMod.MOD_ID);
+
+    public static final RegistryObject<Block> URANIUM_ORE = RegisterBlock("uranium_ore", () -> new DropExperienceBlock(
+            BlockBehaviour.Properties.of(Material.STONE).strength(6f).requiresCorrectToolForDrops(),
+            UniformInt.of(5, 9)), CreativeTabs.TEST_TAB);
 
     public static <T extends Block> RegistryObject<T> RegisterBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> ret = BLOCKS.register(name, block);
